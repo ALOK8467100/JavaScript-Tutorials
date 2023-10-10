3df0eb40f8723d8bf062fb7beb8004c1b62e60ff
Alok kumar <akkumarcse08@gmail.com.com>
2023-10-10 10:28:46 +0530

    1.7 changed

--- 1.DataType/1.7 Type Conversion.js
+++ 1.DataType/1.7 Type Conversion.js
@@ -1,1 +1,10 @@
 // Type Conversion
+let stringType = "55";
+console.log(stringType + " = now it shows that 55 is typestring = " + typeof stringType);
+
+// Number Method
+
+let numberType = Number(stringType);
+console.log(numberType, typeof numberType)
+// In Number method passing String Must be of numeric values
+
